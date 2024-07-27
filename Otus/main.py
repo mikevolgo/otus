0131a75e6d6559e865ce2dd27935b57753816980
Evgeny Zverev <Evgeny.Zverev@kaspersky.com>
2024-07-27 13:56:58 +0300

    test

--- Otus/main.py
+++ Otus/main.py
@@ -2,5 +2,9 @@ def main():
     print("Hello world!!!")
 
 
+def foo(num):
+    return num
+
+
 if __name__ == "__main__":
     main()
--- Otus/tests/test_main.py
+++ Otus/tests/test_main.py
@@ -1,2 +1,9 @@
+from main import foo
+
+
 def test_main():
     assert True
+
+
+def test_foo():
+    assert foo(1) == 1, f"Failed test foo."
